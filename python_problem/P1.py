81ca527f6ba295810e3e73569b14fe74738f2db8
yunjeong <stac_cato@naver.com>
2023-01-06 21:45:53 +0900

    1-8

--- python_problem/P1.py
+++ python_problem/P1.py
@@ -2,8 +2,7 @@
 num = 0
 game_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
     
-
-def player_play():
+def brGame():
     while(True):
         try:
             game_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
@@ -20,27 +19,7 @@ def player_play():
         printing_number('player')
         if num == 31:
             print('computer win!')
-            break        
-
-
-def computer_play():
-    while(True):
-        try:
-            game_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
-            if game_num<1 or game_num>3:
-                print('1,2,3 중 하나를 입력하세요')
-            else: break
-        except ValueError:
-            print('정수를 입력하세요')    
-
-    for i in range(come_num):
-        global num
-        num += 1
-        printing_number('computer')
-        if num == 31:
-            print('computer win!')
-            break        
-
+            break
 
 
 def printing_number(name):
@@ -49,10 +28,6 @@ def printing_number(name):
 
 while(True):
 
-    computer_play()
-    if num == 31:
-        break
-
-    player_play()
-    if num == 31:
-        break
+    brGame()
+        if num == 31:
+            break
